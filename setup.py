8f1464f572fd14cd4da9e48a9f7189377e7529b3
Yurii Sholomii <yuriy.sholomiy@gmail.com>
2018-12-01 14:37:12 +0200

    Init repo
    Add license, gitignore, setup and root module files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='structurizr_py',
+      version='0.1.0',
+      description='Python client library for Structurizr',
+      url='https://github.com/ysholomii/structurizr_py',
+      author='Yurii Sholomii',
+      author_email='yuriy.sholomiy@gmail.com',
+      license='Apache-2.0',
+      packages=['structurizr_py'],
+      zip_safe=False)
