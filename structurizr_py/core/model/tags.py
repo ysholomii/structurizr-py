7652b69fc7e6c72e90ffbbac58d1bab06bb05e2d
Yurii Sholomii <yuriy.sholomiy@gmail.com>
2018-12-16 23:35:03 +0200

    Add Tags
    Add Tags class, representing default tags across a software architecture
    model.

---
+++ structurizr_py/core/model/tags.py
@@ -0,0 +1,15 @@
+class Tags:
+    """Default tags used across a software architecture model."""
+    ELEMENT = "Element"
+    RELATIONSHIP = "Relationship"
+
+    PERSON = "Person"
+    SOFTWARE_SYSTEM = "Software System"
+    CONTAINER = "Container"
+    COMPONENT = "Component"
+
+    DEPLOYMENT_NODE = "Deployment Node"
+    CONTAINER_INSTANCE = "Container Instance"
+
+    SYNCHRONOUS = "Synchronous"  # for styling of synchronous relationships
+    ASYNCHRONOUS = "Asynchronous"  # for styling of asynchronous relationships
