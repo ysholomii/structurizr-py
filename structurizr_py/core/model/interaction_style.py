822992f4c3b42f6c5b4109b2c3a1180469428fbd
Yurii Sholomii <yuriy.sholomiy@gmail.com>
2018-12-16 23:36:29 +0200

    Add InteractionStyle
    Add InteractionStyle which allows to differ synchronous from
    asynchronous style of communication.

---
+++ structurizr_py/core/model/interaction_style.py
@@ -0,0 +1,10 @@
+class InteractionStyle:
+    """
+    Use InteractionStyle on relationships to make the difference between
+    synchronous and asynchronous communication visible.
+    """
+
+    """Denotes synchronous communication."""
+    SYNCHRONOUS = "Synchronous"
+    """Denotes asynchronous communication."""
+    ASYNCHRONOUS = "Asynchronous"
